136dffd992b84d3af89166b389ef635a5011969b
zoikle <134870626+Gros26@users.noreply.github.com>
2025-07-18 17:05:52 -0500

    Ahora se listan los contenedores y se puede acceder a sus carpetas privadas, hay un error al intentar escribir sus llave,valor en el archivo de contenedores

--- myapp/app.py
+++ myapp/app.py
@@ -1,53 +1,74 @@
 from flask import Flask, render_template, jsonify
+import requests
 import os 
 
 app = Flask(__name__)
 
-
 UID = os.environ['HOSTNAME']
-
 NOMBRE_CONTENEDOR = os.environ.get("NOMBRE_CONTENEDOR", UID) 
+#Ruta del archivo compartido entre contenedores
+CONTENEDORES = "/usr/src/app/sync_files/registro/contenedores.txt"
+RUTA_BASE = "/usr/src/app/sync_files/private/"
 
-# Ruta del archivo compartido entre contenedores
-CONTENEDORES = "/usr/src/app/sync_files/contenedores.txt"
 
-
-def registrar_nodo(uid: str, nombre_contenedor: str, archivo: str):
-  """Registra un nodo si no está en el archivo."""
+def registrar_contenedor(uid, nombre_contenedor, archivo):
+  """Registra un contenedor si no está en el archivo."""
   if not os.path.exists(archivo):
-      with open(archivo, "w") as f:
+      with open(archivo, "a") as f:
           f.write(f"{nombre_contenedor}: {uid}\n")
       return
 
   with open(archivo, "r+") as f:
       lineas = f.readlines()
-      ya_registrado = any(linea.startswith(f"{uid}:") for linea in lineas)
+      ya_registrado = any(linea.startswith(f"{nombre_contenedor}:") for linea in lineas)
       if not ya_registrado:
           f.write(f"{nombre_contenedor}: {uid}\n")
 
 
-
-def listar_nodos():
-  """Devuelve un diccionario con todos los nodos registrados."""
-  nodos = {}
+def listar_contenedores():
+  """Devuelve un diccionario con todos los contenedores registrados."""
+  contenedores = {}
   if os.path.exists(CONTENEDORES):
-      with open(CONTENEDORES, "r") as f:
-          for linea in f:
-              if ':' in linea:
-                  clave, valor = linea.strip().split(":", 1)
-                  nodos[clave.strip()] = valor.strip()
-  return nodos
+    with open(CONTENEDORES, "r") as f:
+      for linea in f:
+        if ':' in linea:
+          clave, valor = linea.strip().split(":", 1)
+          contenedores[clave.strip()] = valor.strip()
+  return contenedores
 
 @app.route('/')
 def index():
-  contenedores = listar_nodos()
+  contenedores = listar_contenedores()
   return render_template("index.html", data=contenedores)
 
 
-# Esta ruta listara todos los archivos del contenedor <uid>
-@app.get('/storage/<uid>')
-def listar_archivos_contenedor(uid):
-  return render_template("public.html", data=uid)
+
+
+@app.route("/archivos/<nombre>")
+def listar_archivos_contenedor(nombre):
+  """Este es para que cada contenedor liste sus cosas y despues llamar a este endpoint"""
+  carpeta = os.path.join(RUTA_BASE, nombre)
+
+  if not os.path.exists(carpeta):
+      return jsonify({"error": f"No se encontró la carpeta '{nombre}'"}), 404
+
+  archivos = os.listdir(carpeta)
+  return archivos
+
+
+#Aqui llamamos al endpoint anterior para traer la lista 
+@app.route('/storage/<uid>')
+def listar_archivos_privados(uid):
+    contenedores = listar_contenedores()
+    nombre = None
+
+    for llave, valor in contenedores.items():
+      if valor == uid:
+        nombre = llave
+        break
+
+    response = requests.get(f"http://{nombre}:5000/archivos/{nombre}")
+    return response.json()
 
 
 # Esta ruta permitira listar los archivos publicos de la red
@@ -65,7 +86,6 @@ def bye_world():
   }
 
 if __name__ == '__main__':
-  # Registrar nodo al iniciar
-  registrar_nodo(UID, NOMBRE_CONTENEDOR, CONTENEDORES)
-
+  # Registrar contenedor al iniciar
+  registrar_contenedor(UID, NOMBRE_CONTENEDOR, CONTENEDORES)
   app.run(host='0.0.0.0',port=5000)
